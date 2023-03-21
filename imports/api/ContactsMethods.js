5cde8b1af520f598e9419b3cd5523893e8983ced
juanMaOkorumDev <manuel.marin@okorum.com>
2023-03-20 20:47:39 -0500

    Boton de borrar contacto

--- imports/api/ContactsMethods.js
+++ imports/api/ContactsMethods.js
@@ -1,13 +1,21 @@
 import { ContactsCollection } from "./ContactsCollection";
 import {Meteor} from "meteor/meteor"
+import {check} from "meteor/check";
 
 
 Meteor.methods({ 
     "contacts.insert" ({ name, email, imageUrl }) {
+      check(name, String);
+      check(email, String);
+      check(imageUrl, String);
       if(!name){
         throw new Meteor.Error("El Nombre es obligatorio");
       }
         return  ContactsCollection.insert({ name, email, imageUrl, createdAt: new Date() });
-      } 
+      },
+    "contacts.remove"({contactId}){
+      check(contactId, String);
+      return ContactsCollection.remove(contactId);
+    }   
 });
 
--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { ContactsCollection } from "../api/ContactsCollection";
+import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
 
 export const ContactList = () => {
   const contacts = useTracker(() => {
     return ContactsCollection.find({}, { sort: { createdAt: -1 } }).fetch();
   });
+
+  const removeContact = (event, _id) => {
+    event.preventDefault();
+    Meteor.call("contacts.remove", { contactId: _id });
+  };
 
   return (
     <div>
@@ -37,6 +43,28 @@ export const ContactList = () => {
                   <p className="text-sm font-medium text-gray-500 truncate">
                     {person.email}
                   </p>
+                </div>
+                <div>
+                  <a
+                    href="#"
+                    onClick={(event) => removeContact(event, person._id)}
+                    className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </a>
                 </div>
               </div>
             </li>
